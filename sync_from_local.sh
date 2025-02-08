c346fc59be4791207fd7f27a11ddf7c39988d3bb
axwain <5733365+axwain@users.noreply.github.com>
2025-02-08 01:19:05 -0500

    Updated sync script

--- sync_from_local.sh
+++ sync_from_local.sh
@@ -3,11 +3,26 @@
 HOME_CONFIG=$HOME/.config
 REPO_CONFIG=./.config
 
+echo "Syncing .zshrc"
 rsync -a $HOME/.zshrc ./
+
+echo "Syncing alacritty"
 rsync -a $HOME_CONFIG/alacritty $REPO_CONFIG
+
+echo "Syncing i3"
 rsync -a $HOME_CONFIG/i3 $REPO_CONFIG
+
+echo "Syncing nvim"
 rsync -a $HOME_CONFIG/nvim $REPO_CONFIG
+
+echo "Syncing polybar"
 rsync -a $HOME_CONFIG/polybar $REPO_CONFIG
+
+echo "Syncing rofi"
 rsync -a $HOME_CONFIG/rofi $REPO_CONFIG
+
+echo "Syncing picom"
 rsync -a $HOME_CONFIG/picom.conf $REPO_CONFIG
+
+echo "All operations complete"
 
